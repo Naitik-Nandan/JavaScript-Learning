871aae47bf14f23845b16ab51cacdd8b6e3c9b8d
Naitik-Nandan <naitik.growthomotiv@gmail.com>
2026-04-22 19:51:11 +0000

    Used products.js file to load the products

--- Amazon Project/scripts/amazon.js
+++ Amazon Project/scripts/amazon.js
@@ -1,30 +1,4 @@
-const products = [{
-    image: `athletic-cotton-socks-6-pairs.jpg`,
-    name: `Black and Gray Athletic Cotton Socks - 6 Pairs`,
-    rating: {
-        stars: 4.5,
-        reviews: 87
-    },
-    priceCents: 1090
-},
-{
-    image: `intermediate-composite-basketball.jpg`,
-    name: `Intermediate Size Basketball`,
-    rating: {
-        stars: 4,
-        reviews: 127
-    },
-    priceCents: 2095
-},
-{
-    image: `adults-plain-cotton-tshirt-2-pack-teal.jpg`,
-    name: `Adults Plain Cotton T-Shirt - 2 Pack`,
-    rating: {
-        stars: 4.5,
-        reviews: 56
-    },
-    priceCents: 799
-},];
+
 
 let productsHtml = '';
 
@@ -32,7 +6,7 @@ products.forEach((product) => {
     productsHtml += `<div class="product-container">
           <div class="product-image-container">
             <img class="product-image"
-              src="images/products/${product.image}">
+              src="${product.image}">
           </div>
 
           <div class="product-name limit-text-to-2-lines">
