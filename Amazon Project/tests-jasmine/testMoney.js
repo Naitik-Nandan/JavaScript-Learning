a3ffc8e9b90d2b3439b70cf144bb9e76ba600c8a
NaitikNandan <naitik.growthomotiv@gmail.com>
2026-05-27 13:58:51 +0530

    Removed default tests and added money tests

---
+++ Amazon Project/tests-jasmine/testMoney.js
@@ -0,0 +1,15 @@
+import { money } from "../scripts/utils/money.js";
+
+describe('test suite: money', () => {
+    it('converts paise into rupees', () => {
+        expect(money(2095)).toEqual('20.95');
+    });
+
+    it('works with 0' , () => {
+        expect(money(0)).toEqual('0.00');
+    });
+
+    it('rounds of to the nearest paise' , () => {
+        expect(money(2000.5)).toEqual('20.01');
+    });
+})
--- Amazon Project/tests/testMoney.js
+++ Amazon Project/tests/testMoney.js
@@ -3,23 +3,25 @@ import { money } from "../scripts/utils/money.js";
 console.log('test suite: money')
 console.log('converts paise into rupees')
 //Basic Test
-if((money(0)) === '0.00')
+
+if ((money(2095)) === '20.95')
     console.log('passed');
 else
     console.log('failed');
+
 
 //Edge Cases
 
 console.log('works with 0');
 
-if((money(2095)) === '20.95')
+if ((money(0)) === '0.00')
     console.log('passed');
 else
     console.log('failed');
 
 console.log('rounds up to the nearest paise')
 
-if((money(2000.5)) === '20.01')
+if ((money(2000.5)) === '20.01')
     console.log('passed');
 else
-    console.log('failed')
+    console.log('failed');
